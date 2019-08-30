71f7f0f1acfaed861f38e67c6932974ec48aca48
chelseafood <44086164+chelseafood@users.noreply.github.com>
2019-08-30 11:26:04 +0800

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.Web;
+
+namespace ConsoleApp_PDFFooterAdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fileName = string.Empty;
+
+            DateTime fileCreationDatetime = DateTime.Now;
+
+            fileName = string.Format("{0}.pdf", fileCreationDatetime.ToString(@"yyyyMMdd") + "_" + fileCreationDatetime.ToString(@"HHmmss"));
+            string pdfPath = (@"C:\Users\Chelsea\Documents\") + fileName;
+            //string pdfPath = HttpContext.Current.Server.MapPath(@"C:\Users\Chelsea\Documents") + fileName;
+            //@{ "C:\Users\Chelsea\Documents"}
+            //Server.MapPath(@"~\PDFs\")
+            using (FileStream msReport = new FileStream(pdfPath, FileMode.Create))
+            {
+                //step 1
+                using (Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 140f, 10f))
+
+                {
+                    try
+                    {
+                        // step 2
+                        PdfWriter pdfWriter = PdfWriter.GetInstance(pdfDoc, msReport);
+                        pdfWriter.PageEvent = new ITextEvents();
+
+                        //open the stream 
+                        pdfDoc.Open();
+
+                        for (int i = 0; i < 10; i++)
+                        {
+                            Paragraph para = new Paragraph("Hello world. Checking Header Footer", new Font(Font.FontFamily.HELVETICA, 22));
+
+                            para.Alignment = Element.ALIGN_CENTER;
+
+                            pdfDoc.Add(para);
+
+                            pdfDoc.NewPage();
+                        }
+
+                        pdfDoc.Close();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        //handle exception
+                    }
+
+                    finally
+                    {
+
+
+                    }
+                }
+
+            }
+        }
+    }
+}
